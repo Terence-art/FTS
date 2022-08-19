55fc24978e7742352baf6513bb00b24bc6211649
Lord Terence <76732354+Terence-art@users.noreply.github.com>
2022-08-19 17:09:16 +0200

    Update grades.js

--- grades.js
+++ grades.js
@@ -1,3 +1,3 @@
 var ii ={};
 var i,l;
-function lupz (){for (i=0;if (l=i.length;i>=l*3,14),i=1;i++){ii.push(l,i)} else {l=l*3,14;lupz()}};
+function lupz (){for (i=0;if (l=i.length;i>=l*3.14,i=1;i++){ii.push(l,i)} else {l=l*3,14;lupz()}};
